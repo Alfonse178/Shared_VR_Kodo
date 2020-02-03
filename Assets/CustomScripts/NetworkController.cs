f43bfbbe29ab890d0a4ee6b9105b4adcc0ada144
Alfie Sargent <Alfie@learnkodo.com>
2020-02-03 13:05:51 -0300

    Working connection to SA servers using PUN

--- Assets/CustomScripts/NetworkController.cs
+++ Assets/CustomScripts/NetworkController.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class NetworkController : MonoBehaviour
+public class NetworkController : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        PhotonNetwork.ConnectUsingSettings(); //connceting to photon master server, should be the best one
     }
 
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("We are connected to the " + PhotonNetwork.CloudRegion + " Server!");
+    }
     // Update is called once per frame
     void Update()
     {
