374d305f19c25c885cd44150d7f0361de950e598
Alfie Sargent <Alfie@learnkodo.com>
2020-02-04 17:21:07 -0300

    2 players now fully working, build to oculus

--- Assets/CustomScripts/CharacterCon.cs
+++ Assets/CustomScripts/CharacterCon.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class CharacterCon : MonoBehaviour
+public class CharacterCon : MonoBehaviourPunCallbacks
 {
 
     public float speed = 10.0f;
     private float translation;
     private float straffe;
     private PhotonView PV;
+    [SerializeField] private Camera m_Camera;
 
     // Use this for initialization
     void Start()
@@ -17,16 +18,22 @@ public class CharacterCon : MonoBehaviour
         // turn off the cursor
         PV = GetComponent<PhotonView>();
         Cursor.lockState = CursorLockMode.Locked;
+        if (!PV.IsMine)
+        {
+            //Destroy(m_Camera);
+            m_Camera.enabled = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+
         // Input.GetAxis() is used to get the user's input
         // You can furthor set it on Unity. (Edit, Project Settings, Input)
-        if (!PV.IsMine)
+        if (PV.IsMine)
         {
-            //Debug.Log("passed the if statement");
+            Debug.Log("passed the if statement");
         translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         straffe = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         transform.Translate(straffe, 0, translation);
--- Assets/CustomScripts/MouseCamLook.cs
+++ Assets/CustomScripts/MouseCamLook.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class MouseCamLook : MonoBehaviour
+public class MouseCamLook : MonoBehaviourPunCallbacks
 {
 
     [SerializeField]
